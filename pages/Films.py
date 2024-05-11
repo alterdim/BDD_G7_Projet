bfaff2d9ecd4c4eb2a061663313c06e66780a686
Alternis <louis@gerard.li>
2024-05-11 18:19:13 +0200

    movie_genders

--- pages/Films.py
+++ pages/Films.py
@@ -5,16 +5,37 @@ from common import *
 
 dbname = "b8syyge5j3lftmivshyv"
 
-#  Interface utilisateur de Streamlit
-st.title("Vos cinémas")
-
-# Fonction pour obtenir des produits de la base de données
 def get_movies():
     connection = connect_to_db2(dbname)
     query = "SELECT * FROM film"
     data = pd.read_sql(query, connection)
     connection.close()
     return data
+
+def filter_movies_gender(gender_id):
+    connection = connect_to_db2(dbname)
+    query = "SELECT * FROM produit WHERE genre1 = %d OR genre2 = %d"
+    data = pd.read_sql(query, connection, params=(gender_id,))
+    connection.close()
+    return data
+
+def get_genders():
+    connection = connect_to_db2(dbname)
+    query = "SELECT * FROM genre"
+    data = pd.read_sql(query, connection, params=())
+    connection.close()
+    return data
+
 if st.button('Afficher Cinémas'):
     products_df = get_movies()
     st.write(products_df)
+
+    
+#  Interface utilisateur de Streamlit
+st.title("Vos cinémas")
+
+# Recherche de film par genre(s)
+st.subheader("Rechercher un film par genre")
+movie_genders = get_genders()
+product_id2 = st.selectbox("TEst", movie_genders)
+
